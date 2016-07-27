f5aca77cff0c5839fb955227fd9c02ee9ece2ac1
dorapon <dorapon2000@gmail.com>
2016-07-28 04:26:28 +0900

    Change rule to byte-compile

---
+++ byte-compile.el
@@ -0,0 +1,3 @@
+(package-initialize)
+(add-to-list 'load-path "~/.emacs.d/elisp/auto-complete")
+(byte-recompile-directory (expand-file-name "~/.emacs.d/inits") 0)
--- byte-compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-#initsディレクトリ内で使うようにする
-emacs -batch -f batch-byte-compile inits/*.el
