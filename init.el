e302840ac4f85a8fd64fa98d1567a62cf4fa2391
dorapon <dorapon2000@gmail.com>
2016-07-28 04:31:08 +0900

    Remove duplicate code

--- init.el
+++ init.el
@@ -1,7 +1,6 @@
-(require 'package)
-(setq package-user-dir "~/.emacs.d/elpa/") ;; これは好みに合わせて
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(package-initialize)
+;;; init-loader
+;;; inits以下の設定ファイルを読み込む
+;;; OSによって読み込むファイルを自動で切り替える
 
 (require 'init-loader)
 (setq init-loader-show-log-after-init nil)
